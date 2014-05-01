b58f55d53d0f73b29bd2087a043bb4f2104e562a
Timothy Wang <timothy.wang@zefr.com>
2014-05-01 11:30:49 -0700

    Add basic click and move parking

--- tandem.js
+++ tandem.js
@@ -1,8 +1,42 @@
 (function() {
-  $('.car').click(function() {
-    $(this).toggleClass('parked');
-  });
-  $('.car').hover(function() {
-    $(this).toggleClass('requested');
-  });
+
+  function TandemSpot(car) {
+    var self = this;
+    self.car = ko.observable(car || null);
+
+    self.park = function(car) {
+      if (car) {
+        if (car.spot())
+          car.spot().car(null);
+        car.spot(this);
+        this.car(car);
+      }
+    }
+  }
+
+  function Car(driver) {
+    var self = this;
+    self.driver = ko.observable(driver || null);
+    self.spot = ko.observable();
+  }
+
+  function TandemViewModel() {
+    var self = this;
+    self.cars = ko.observableArray([
+    ]);
+    self.spots = ko.observableArray([
+      new TandemSpot(),
+      new TandemSpot()
+    ]);
+    self.car = ko.observable();
+
+    self.selectCar = function(car) {
+      if (car == self.car())
+        self.car(null);
+      else
+        self.car(car);
+    }
+  };
+
+  ko.applyBindings(new TandemViewModel());
 })();
