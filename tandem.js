2cda086bfa49a48df2a9a1ceb5a15895dbc49029
Timothy Wang <timothy.wang@zefr.com>
2014-05-01 16:26:22 -0700

    Allow requests when parked

--- tandem.js
+++ tandem.js
@@ -6,43 +6,48 @@
     self.requests = ko.observableArray([]);
 
     self.request = function(car) {
-      if (!car || self.parked()) return;
+      if (!car || self.parked() == car) return;
       if (self.requests.indexOf(car) < 0) {
-        if (car.spot())
-          car.spot().requests.remove(car);
-        car.spot(self);
+        if (car.request())
+          car.request().requests.remove(car);
+        car.request(self);
         self.requests.push(car);
       } else {
-        car.spot(null);
+        car.request(null);
         self.requests.remove(car);
       }
     }
 
     self.carHeight = ko.computed(function() {
-      return 100/self.requests().length + '%';
+      var numCars = self.requests().length;
+      if (self.parked()) numCars++;
+      return 100/numCars + '%';
     });
   }
 
   function Car(driver) {
     var self = this;
     self.driver = ko.observable(driver || null);
-    self.spot = ko.observable();
-    self.parked = ko.observable(false);
+    self.request = ko.observable();
+    self.parked = ko.observable();
+    self.streetParked = ko.observable(false);
 
     self.park = function() {
-      if (!self.spot()) {
-        // Toggle street parking
-        self.parked(!self.parked());
-        return;
-      }
-      if (!self.parked()) {
-        self.parked(true);
-        self.spot().requests.remove(self);
-        self.spot().parked(self);
+      if (self.request() && !self.request().parked()) {
+        self.streetParked(false);
+        if (self.parked()) {
+          self.parked().parked(null);
+        }
+        self.request().requests.remove(self);
+        self.request().parked(self);
+        self.parked(self.request());
+        self.request(null);
+      } else if (self.parked()) {
+        self.streetParked(false);
+        self.parked().parked(null);
+        self.parked(null);
       } else {
-        self.parked(false);
-        self.spot().parked(null);
-        self.spot(null);
+        self.streetParked(!self.streetParked());
       }
     }
   }
