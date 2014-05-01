92e0b7505a2c24abfb4ed9b17de8e6bb15aa6731
Timothy Wang <timothy.wang@zefr.com>
2014-05-01 12:17:08 -0700

    Add request spot feature

--- tandem.js
+++ tandem.js
@@ -3,6 +3,18 @@
   function TandemSpot(car) {
     var self = this;
     self.car = ko.observable(car || null);
+    self.requests = ko.observableArray([]);
+
+    self.request = function(car) {
+      if (self.requests.indexOf(car) < 0) {
+        if (car.request())
+          car.request().requests.remove(car);
+        car.request(self);
+        self.requests.push(car);
+      } else {
+        self.requests.remove(car);
+      }
+    }
 
     self.park = function(car) {
       if (car) {
@@ -12,12 +24,17 @@
         this.car(car);
       }
     }
+
+    self.carHeight = ko.computed(function() {
+      return 100/self.requests().length + '%';
+    });
   }
 
   function Car(driver) {
     var self = this;
     self.driver = ko.observable(driver || null);
     self.spot = ko.observable();
+    self.request = ko.observable();
   }
 
   function TandemViewModel() {
