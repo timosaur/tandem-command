bdf68ba542eddf368ddbb9ad9e33442a843b8f99
Timothy Wang <timothy.wang@zefr.com>
2014-05-07 11:34:26 -0700

    Add basic save/load feature using Twitter
    * Extract data from twitter widget feed to load
    * Save by letting using tweet to @TandemCommander

--- tandem.js
+++ tandem.js
@@ -56,13 +56,8 @@
 
   function TandemViewModel() {
     var self = this;
-    self.cars = ko.observableArray([
-    ]);
-
-    self.spots = ko.observableArray([
-      new TandemSpot(),
-      new TandemSpot()
-    ]);
+    self.cars = ko.observableArray([]);
+    self.spots = ko.observableArray([]);
     self.car = ko.observable();
 
     self.time = ko.observable();
@@ -126,6 +121,120 @@
       else
         self.car(car);
     }
+
+    self.save = function() {
+      console.log("save");
+      var saveData = [];
+      saveData.push(self.spots().length);
+      saveData.push(self.cars().length);
+      ko.utils.arrayForEach(self.cars(), function(car) {
+        saveData.push(car.driver());
+        saveData.push(car.time() || "");
+        if (car.parked() === "street") {
+           saveData.push("s");
+        } else {
+          var spotLoc = self.spots.indexOf(car.parked());
+          if (spotLoc !== -1) saveData.push(spotLoc);
+          else saveData.push("");
+        }
+      });
+      window.open("https://twitter.com/intent/tweet?screen_name=TandemCommander&text=save "+
+                  saveData.join(","));
+    }
+
+    $.ajax({
+      url: "https://cdn.syndication.twimg.com/widgets/timelines/463924049412771840",
+      method: "get",
+      dataType: "jsonp"
+
+    }).success(function(data) {
+      var parser = document.createElement('div');
+      parser.innerHTML = data['body'];
+      var tweets = $(parser).find('.e-entry-title');
+      var parkingUpdates = {};
+      for (var i in tweets) {
+        var tweet = tweets[i].innerText;
+        console.log("data", tweet);
+
+        /* Check for parking updates */
+        var parkStart = tweet.indexOf("park");
+        if (parkStart != -1) {
+          var parkData = tweet.slice(parkStart+5, tweet.length).split(" ");
+          var driver = parkData[0];
+          if (!(driver in parkingUpdates)) {
+            var spot = parkData[1];
+            if (spot !== "street") {
+              spot = parseInt(spot);
+            }
+            parkingUpdates[driver] = spot;
+          }
+          continue;
+        }
+        var goneStart = tweet.indexOf("gone");
+        if (goneStart != -1) {
+          var driver = tweet.slice(goneStart+5, tweet.length);
+          if (!(driver in parkingUpdates)) {
+            parkingUpdates[driver] = "gone";
+          }
+          continue;
+        }
+
+        /* Check for save data */
+        var saveStart = tweet.indexOf("save");
+        if (saveStart != -1) {
+          var saveDataa = tweet.slice(saveStart+5, tweet.length).split(",");
+          var dataIdx = 0;
+          var numSpots = parseInt(saveDataa[dataIdx++]);
+          console.log("saved spots:", numSpots);
+          for (; numSpots>0; numSpots--) {
+            self.spots.push(new TandemSpot());
+          }
+          var numCars = parseInt(saveDataa[dataIdx++]);
+          console.log("saved cars:", numCars);
+          for (; numCars>0; numCars--) {
+            var carData = {
+              name: saveDataa[dataIdx++],
+              time: saveDataa[dataIdx++],
+              parked: saveDataa[dataIdx++]
+            }
+            var car = new Car(carData.name);
+            if (carData.time !== "")
+              car.time(parseInt(carData.time));
+            if (carData.parked) {
+              console.log("park", carData.name, carData.parked);
+              if (carData.parked === "s") {
+                car.parked("street");
+              } else {
+                var parkedSpot = self.spots()[parseInt(carData.parked)];
+                car.parked(parkedSpot);
+                parkedSpot.parked(car);
+              }
+            }
+            console.log("loaded", car.driver());
+            self.cars.push(car);
+          }
+          /* Save found, no more data required */
+          break;
+        }
+      }
+
+      /* Update parking spots */
+      for (var driver in parkingUpdates) {
+        var spot = parkingUpdates[driver];
+        console.log("park", driver, spot);
+        var car = ko.utils.arrayFirst(self.cars(), function(car) {
+          return car.driver() === driver;
+        })
+        if (car.parked() && car.parked() !== "street")
+          car.parked().parked(null);
+        car.parked(null);
+        if (spot !== "gone") {
+          var newSpot = self.spots()[spot];
+          car.parked(newSpot);
+          newSpot.parked(car);
+        }
+      }
+    })
   };
 
   ko.applyBindings(new TandemViewModel());
